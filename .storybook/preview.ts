1b54393e763584f634500dfa768eba2726af30d5
Miguel Pinto <migueljcpinto@gmail.com>
2025-09-05 01:43:24 +0200

    Update storybook preview configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,6 @@
 import type { Preview } from "@storybook/nextjs-vite";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import "../app/globals.css";
-import { withThemeByClassName } from "@storybook/addon-themes";
 
 const preview: Preview = {
 	decorators: [
