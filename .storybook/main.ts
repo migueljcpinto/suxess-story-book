34b3e57648980af2d928e67fef457d731607ec30
Miguel Pinto <migueljcpinto@gmail.com>
2025-09-05 01:18:39 +0200

    revert some corrections

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-	stories: [
-		"../**/*.mdx",
-		"../**/*.stories.@(js|jsx|mjs|ts|tsx)",
-		"../stories/**/*.mdx",
-		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-	],
+	stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
 		"@chromatic-com/storybook",
 		"@storybook/addon-docs",
